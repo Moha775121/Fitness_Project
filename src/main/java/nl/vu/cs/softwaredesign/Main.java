d46220918688ee35b24959a4afe20990f271ce9d
XED <m.jb@MJs-MacBook-Air.local>
2026-03-16 07:53:49 +0100

    [data] User profile CLI implementation

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,9 +1,29 @@
 package nl.vu.cs.softwaredesign;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import nl.vu.cs.softwaredesign.domain.core.UserProfile;
+import nl.vu.cs.softwaredesign.repository.ExerciseRepository;
+import nl.vu.cs.softwaredesign.repository.JsonExerciseRepository;
+import nl.vu.cs.softwaredesign.ui.UserProfileCLI;
+
+import java.util.Scanner;
 
 public class Main {
-    public void helper() {}
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-    public static void main (String[] args) {
-        System.out.println("Welcome to Software Design!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        UserProfileCLI cli = new UserProfileCLI();
+        UserProfile user = cli.promptForProfile(scanner);
+
+        ExerciseRepository repo = new JsonExerciseRepository("src/main/resources/exercises.json");
+
+        LOGGER.log(Level.INFO, "Loaded {0} exercises from the database.", repo.getAll().size());
+        LOGGER.log(Level.INFO, user::toString);
+        LOGGER.log(Level.INFO, "System is ready for the Planning Engine...");
+
+        scanner.close();
     }
 }
---
+++ src/main/java/nl/vu/cs/softwaredesign/ui/UserProfileCLI.java
@@ -0,0 +1,38 @@
+package nl.vu.cs.softwaredesign.ui;
+
+import nl.vu.cs.softwaredesign.domain.core.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UserProfileCLI {
+
+    // We pass the Scanner in so we can mock it later for SonarQube testing!
+    // This is CLI Sytem.out and I know it.
+    @SuppressWarnings("java:S106")
+    public UserProfile promptForProfile(Scanner scanner) {
+        System.out.println("=== Welcome to the Fitness Training Planner ===");
+
+        System.out.print("Enter your weight in kg: ");
+        double weight = Double.parseDouble(scanner.nextLine());
+
+        System.out.print("Enter average calories consumed per day: ");
+        int calories = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter average steps per day: ");
+        int steps = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter days available to train per week (1-7): ");
+        int days = Integer.parseInt(scanner.nextLine());
+
+        // KISS
+        Goal goal = new Goal(12, 75.0, GoalType.WEIGHT_LOSS);
+
+        // Initialise list.
+        Constraint constraint = new Constraint(days, 60, new ArrayList<>());
+
+
+        System.out.println("Profile created successfully!");
+
+        return new UserProfile(weight, calories, steps, days, Sex.MALE, goal, constraint);
+    }
+}

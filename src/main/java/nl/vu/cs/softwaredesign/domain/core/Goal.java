00f59eb4bf5bedbe97cff5085bc0f98ebe0cb3db
Kelvin-18449 <152967885+Kelvin-18449@users.noreply.github.com>
2026-03-17 10:03:16 +0100

    "Defined what the goal class does"

--- src/main/java/nl/vu/cs/softwaredesign/domain/core/Goal.java
+++ src/main/java/nl/vu/cs/softwaredesign/domain/core/Goal.java
@@ -1,5 +1,5 @@
 package nl.vu.cs.softwaredesign.domain.core;
-
+// A class representing a user's fitness goal, which includes the target number of weeks to achieve the goal.
 public class Goal {
     private int targetWeeks;
     private double targetWeight;
